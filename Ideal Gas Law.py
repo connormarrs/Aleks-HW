775c91e0564d4f91f9fa7fa5def36a55f071d597
Connor Marrs <connormarrs@Connor-Marrs.local>
2015-06-01 19:40:31 -0400

    This is a working code that states the volume of a gas.

---
+++ Ideal Gas Law.py
@@ -0,0 +1,12 @@
+#Connor Marrs May 18, 2015
+#In this program, I am trying to create an ideal gas law calculator.
+
+gasname = input("What is the name of the gas?:")
+
+def idealgas(atm,mol,kel):
+    idealgas = float(kel)*float(mol)*float(0.08206)/float(atm)
+    return (idealgas)
+
+vol=idealgas(1,5,500)
+a="The volume of %s is %20f." % (gasname,vol)
+print a
