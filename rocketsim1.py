d55e8237102d5350e34bc2c5d3bba5536bfc131e
Connor Marrs <marrs.connor.1@gmail.com>
2015-09-05 14:40:31 -0400

    This is a working rocket simulator that can successfully compute time of flight in 1.41 seconds.

--- rocketsim1.py
+++ rocketsim1.py
@@ -6,44 +6,65 @@ x=list()
 ge=list()
 gm=list()
 
-#define constants as variables (g, mi)
+#define constants as variables (g, mr)
 g = 6.674*10**(-11)
 moon = 7.347*10**(22)
 earth = 5.972*10**(24)
 leo = 6531
 dtotal = 392508000
-mi = 5200
+mr = 5200
 fuelbr = 1.16*10**(-2) #This is the rate at which our rocket expends fuel in kg/s.
-thrust = 10**8
+tr = 4.4*10**8
 vi = 11200
 
+#Here, we set the initial values and conditions
 x.extend([160000])
 
-gei = g*earth*mi/(x[0])**2
-gmi = g*moon*mi/(dtotal-x[0])**2
+gei = g*earth*mr/(x[0])**2
+gmi = g*moon*mr/(dtotal-x[0])**2
+a0 = (tr-gei+gmi)/mr
 
-accel.extend([0])
+accel.extend([a0])
 vel.extend([vi])
 ge.extend([gei])
 gm.extend([gmi])
 
 #define dt, general unit of time for program
-dt = 0.1 #0.1 seconds
+dt = 1 #0.1 seconds
 
 #initialize time list and fill
-time = list(np.arange(0,40,dt))
+time = list(np.arange(0,400,dt))
 
 #define position, velocity, acceleration, and time dependent quantities
-##for i in time:
-##    mr = mi - fuelbr*i#This is the mass of the rocket as time goes on
-##    a = (g*moon*mr + g*earth*mr )/mr
-##    v = accel*i
-##    x = vel*i
+for i in range(1,len(time)+1):
+    #Here we find the next position
+    print
+    print"This is loop", i
+    print
+    print"These are the positions", x
+    print
+    print"These are the velocities", vel
+    print
+    print"These are the accelerations", accel
+    print
+    vnew=vel[i-1]+accel[i-1]*dt
+    vel.extend([vnew])
+    xnew=x[i-1]+vel[i-1]*dt
+    x.extend([xnew])
+    genew=g*earth*mr/(x[i])**2
+    ge.extend([genew])
+    gmnew=g*moon*mr/(dtotal-x[i])**2
+    gm.extend([gmnew])
+    accelnew=(tr+gm[i]-ge[i])/mr
+    accel.extend([accelnew])
+    
+    #detect index of timestamp when velocity and acceleration are at zero. take     this quantity and print it
+    timefinal=time[i]
 
-#begin to fill 3 main lists
-
-
-#define functions and call in for loop for 400000 dt
-
-
-#detect index of timestamp when velocity and acceleration are at zero. take this quantity and print it
+    if x[-1]>dtotal:
+        print"time=", timefinal
+        print
+        print
+        print
+        print
+        break
