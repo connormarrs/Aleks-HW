ec89cacd3b38ba822f2dad9847e000a98a481c17
Connor Marrs <marrs.connor.1@gmail.com>
2015-09-19 13:50:44 -0400

    This is a working sandbox for out rocket simulator graphing function

--- Plotting Sandbox.py
+++ Plotting Sandbox.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
-x=[1,2,3,4]
-y=[1,4,8,16]
-plt.plot(x, y, 'yx', )
-plt.axis([0, 6, 0, 20])
+x=[0,1,2,3,4,5,6,10]
+y=[0,1,4,8,16,25,36,100]
+plt.plot(x,y,marker='o', linestyle='-', color='r', label='Rocket Velocity')
+# plt.axis([0, 6, 0, 36])
+plt.title("y=x^2")
+plt.margins(0.1)
+plt.xlabel("label")
+plt.ylabel("otherlabel")
+plt.legend(loc="best")
 plt.show()
